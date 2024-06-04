9dfe3f4ba259f8fec13ead6e4a5ee861ef8540cc
zewditu Hailemariam <zehailem@microsoft.com>
2024-06-03 17:39:49 -0700

    Add sample  for zero memery chat

---
+++ src/quickstarts/semantic-kernel/Kernel/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.SemanticKernel;
+string openAIChatCompletionModelName = "gpt-3.5-turbo";
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(openAIChatCompletionModelName, Environment.GetEnvironmentVariable("OPENAI_API_KEY")) // add the OpenAI chat completion service.
+    .Build();
+
+// Basic chat
+// This is zero memory or stateless chat. The AI will not remember anything from the previous messages.
+while (true)
+{
+    Console.Write("Q': ");
+    Console.WriteLine(await kernel.InvokePromptAsync(Console.ReadLine()!));
+}
