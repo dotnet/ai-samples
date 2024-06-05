a2d849f1a2d071aedc4a84eba95b16c579c43e32
zewditu Hailemariam <zehailem@microsoft.com>
2024-06-05 14:37:56 -0700

    Added sample project.

---
+++ src/quickstarts/semantic-kernel/HelloSemanticKernel/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.SemanticKernel;
+string openAIChatCompletionModelName = "gpt-3.5-turbo";// // this could be other models like "gpt-4-turbo".
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(openAIChatCompletionModelName, Environment.GetEnvironmentVariable("OPENAI_API_KEY")) // add the OpenAI chat completion service.
+    .Build();
+
+// Basic chat
+// This is zero memory or stateless chat. The AI will not remember anything from the previous messages.
+while (true)
+{
+    Console.Write("Q: ");
+    Console.WriteLine(await kernel.InvokePromptAsync(Console.ReadLine()!));
+}
