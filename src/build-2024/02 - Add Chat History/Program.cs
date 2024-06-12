765173f620f270126108a3272fdb0d09d99f5788
zewditu Hailemariam <36615490+zewditu@users.noreply.github.com>
2024-06-12 10:03:47 -0700

    Update Program.cs
    Review fix

--- src/build-2024/02 - Add Chat History/Program.cs
+++ src/build-2024/02 - Add Chat History/Program.cs
@@ -12,8 +12,8 @@ ChatHistory chatHistory = [];
 while (true)
 {
     Console.Write("Q: ");
-    chatHistory.AddUserMessage(Console.ReadLine());// Add user message to chat history, then it can be use to get more context for the next chat response
-    var response = await chatService.GetChatMessageContentsAsync(chatHistory);// Get chat response based on chat history
-    Console.WriteLine(response[response.Count - 1]);
+    chatHistory.AddUserMessage(Console.ReadLine()); // Add user message to chat history.
+    var response = await chatService.GetChatMessageContentsAsync(chatHistory); // Get chat response based on chat history.
+    Console.WriteLine(response); // Print response.
     chatHistory.AddRange(response);// Add chat response to chat history, hence it can be use to get more context for the next chat response
 }
