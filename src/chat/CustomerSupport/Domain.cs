90be2260ef6a38eb0269c3d8718ac27406640ce0
luisquintanilla <Luis.Quintanilla@microsoft.com>
2024-10-15 11:56:43 -0400

    Initial commit

--- src/chat/CustomerSupport/Domain.cs
+++ src/chat/CustomerSupport/Domain.cs
@@ -1,4 +1,5 @@
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
+using Microsoft.Extensions.AI;
 
 public class ManualChunk
 {
@@ -6,7 +7,7 @@ public class ManualChunk
     public int ProductId { get; set; }
     public int PageNumber { get; set; }
     public required string Text { get; set; }
-    public required float[] Embedding { get; set; }
+    public required Embedding<float> Embedding { get; set; }
 }
 
 public class Message
--- src/chat/CustomerSupport/ProductManualSemanticSearch.cs
+++ src/chat/CustomerSupport/ProductManualSemanticSearch.cs
@@ -1,15 +1,16 @@
-#pragma warning disable
+﻿#pragma warning disable
 using System.Numerics.Tensors;
+using Microsoft.Extensions.AI;
 using Microsoft.SemanticKernel.Embeddings;
 
 public class ProductManualSemanticSearch
 {
-    private readonly ITextEmbeddingGenerationService _embeddingService;
+    private readonly IEmbeddingGenerator<string,Embedding<float>> _embeddingGenerator;
     private readonly IEnumerable<ManualChunk> _data;
 
-    public ProductManualSemanticSearch(ITextEmbeddingGenerationService embeddingService, IEnumerable<ManualChunk> data)
+    public ProductManualSemanticSearch(IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator, IEnumerable<ManualChunk> data)
     {
-        _embeddingService = embeddingService;
+        _embeddingGenerator = embeddingGenerator;
         _data = data;
     }
 
@@ -18,16 +19,16 @@ public class ProductManualSemanticSearch
         Console.WriteLine($"Searching for '{query}' in product {productId}...");
 
         // [1] Embed query
-        var queryEmbedding = await _embeddingService.GenerateEmbeddingsAsync(new[] {query});
+        var queryEmbedding = await _embeddingGenerator.GenerateAsync(new[] {query});
 
         // [2] Embed data
         var dataEmbeddings = _data
             .Where(d => d.ProductId == productId)
-            .Select(d => d.Embedding);
+            .Select(d => d.Embedding.Vector);
 
         // [3] Compute similarity
         var similarities = dataEmbeddings
-            .Select(e => TensorPrimitives.CosineSimilarity(queryEmbedding.First().Span,e.AsSpan()))
+            .Select(e => TensorPrimitives.CosineSimilarity(queryEmbedding.First().Vector.Span,e.Span))
             .ToList();
 
         // [4] Sort by similarity
