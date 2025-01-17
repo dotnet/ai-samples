615ee692056d82c883c6b7b84a3b03da1d3506c5
Shyam Namboodiripad <gnamboo@microsoft.com>
2025-01-17 14:07:32 -0800

    Some fixes for MEAI.Evaluation sample
    Fix samples to work correctly against Ollama and other LLM providers.
    
    Also added some comments.

--- src/microsoft-extensions-ai-evaluation/api/evaluation/Setup/TestSetup.cs
+++ src/microsoft-extensions-ai-evaluation/api/evaluation/Setup/TestSetup.cs
@@ -29,53 +29,116 @@ public class TestSetup
 
     private static ChatConfiguration GetAzureAIInferenceChatConfiguration()
     {
+        /// Get an instance of Microsoft.Extensions.AI's <see cref="IChatClient"/> interface for the selected LLM
+        /// endpoint.
         IChatClient client =
             new ChatCompletionsClient(
                 new Uri(EnvironmentVariables.AzureAIInferenceEndpoint),
                 new AzureKeyCredential(EnvironmentVariables.AzureAIInferenceAPIKey))
                     .AsChatClient(modelId: EnvironmentVariables.AzureAIInferenceModel);
 
-        IEvaluationTokenCounter tokenCounter =
+        IEvaluationTokenCounter? tokenCounter = null;
+        if (EnvironmentVariables.AzureAIInferenceModelInputTokenLimit.HasValue)
+        {
+            /// Note that while <see cref="TiktokenTokenizer"/> supports a wide variety of models, it may not (yet)
+            /// support the model you have selected. In this case, you can either turn off the token counting for all
+            /// evaluations performed in the included examples (by unsetting the above environment variable that
+            /// specifies the token limit), or you can implement <see cref="IEvaluationTokenCounter"/> for the selected
+            /// model (by wrapping any other tokenizer API that that supports the selected model) and pass this
+            /// <see cref="IEvaluationTokenCounter"/> down to the <see cref="ChatConfiguration"/> created below.
+            tokenCounter =
             TiktokenTokenizer.CreateForModel(EnvironmentVariables.AzureAIInferenceModel)
-                .ToTokenCounter(EnvironmentVariables.AzureAIInferenceModelInputTokenLimit);
+                    .ToTokenCounter(EnvironmentVariables.AzureAIInferenceModelInputTokenLimit.Value);
+        }
 
+        /// Create an instance of Microsoft.Extensions.AI.Evaluation's <see cref="ChatConfiguration"/>. All the
+        /// evaluations performed in the included examples will use this <see cref="ChatConfiguration"/> to communicate
+        /// with the LLM.
         return new ChatConfiguration(client, tokenCounter);
     }
 
     private static ChatConfiguration GetAzureOpenAIChatConfiguration()
     {
+        /// Get an instance of Microsoft.Extensions.AI's <see cref="IChatClient"/> interface for the selected LLM
+        /// endpoint.
         IChatClient client =
             new AzureOpenAIClient(new Uri(EnvironmentVariables.AzureOpenAIEndpoint), new DefaultAzureCredential())
                 .AsChatClient(modelId: EnvironmentVariables.AzureOpenAIModel);
 
-        IEvaluationTokenCounter tokenCounter =
+        IEvaluationTokenCounter? tokenCounter = null;
+        if (EnvironmentVariables.AzureOpenAIModelInputTokenLimit.HasValue)
+        {
+            /// Note that while <see cref="TiktokenTokenizer"/> supports a wide variety of models, it may not (yet)
+            /// support the model you have selected. In this case, you can either turn off the token counting for all
+            /// evaluations performed in the included examples (by unsetting the above environment variable that
+            /// specifies the token limit), or you can implement <see cref="IEvaluationTokenCounter"/> for the selected
+            /// model (by wrapping any other tokenizer API that that supports the selected model) and pass this
+            /// <see cref="IEvaluationTokenCounter"/> down to the <see cref="ChatConfiguration"/> created below.
+            tokenCounter =
             TiktokenTokenizer.CreateForModel(EnvironmentVariables.AzureOpenAIModel)
-                .ToTokenCounter(EnvironmentVariables.AzureOpenAIModelInputTokenLimit);
+                    .ToTokenCounter(EnvironmentVariables.AzureOpenAIModelInputTokenLimit.Value);
+        }
 
+        /// Create an instance of Microsoft.Extensions.AI.Evaluation's <see cref="ChatConfiguration"/>. All the
+        /// evaluations performed in the included examples will use this <see cref="ChatConfiguration"/> to communicate
+        /// with the LLM.
         return new ChatConfiguration(client, tokenCounter);
     }
 
     private static ChatConfiguration GetOllamaChatConfiguration()
     {
-        IChatClient client = new OllamaChatClient(new Uri(EnvironmentVariables.OllamaEndpoint));
+        /// Get an instance of Microsoft.Extensions.AI's <see cref="IChatClient"/> interface for the selected LLM
+        /// endpoint.
+        IChatClient client =
+            new OllamaChatClient(
+                new Uri(EnvironmentVariables.OllamaEndpoint),
+                modelId: EnvironmentVariables.OllamaModel);
 
-        IEvaluationTokenCounter tokenCounter =
+        IEvaluationTokenCounter? tokenCounter = null;
+        if (EnvironmentVariables.OllamaModelInputTokenLimit.HasValue)
+        {
+            /// Note that while <see cref="TiktokenTokenizer"/> supports a wide variety of models, it may not (yet)
+            /// support the model you have selected. In this case, you can either turn off the token counting for all
+            /// evaluations performed in the included examples (by unsetting the above environment variable that
+            /// specifies the token limit), or you can implement <see cref="IEvaluationTokenCounter"/> for the selected
+            /// model (by wrapping any other tokenizer API that that supports the selected model) and pass this
+            /// <see cref="IEvaluationTokenCounter"/> down to the <see cref="ChatConfiguration"/> created below.
+            tokenCounter =
             TiktokenTokenizer.CreateForModel(EnvironmentVariables.OllamaModel)
-                .ToTokenCounter(EnvironmentVariables.OllamaModelInputTokenLimit);
+                    .ToTokenCounter(EnvironmentVariables.OllamaModelInputTokenLimit.Value);
+        }
 
+        /// Create an instance of Microsoft.Extensions.AI.Evaluation's <see cref="ChatConfiguration"/>. All the
+        /// evaluations performed in the included examples will use this <see cref="ChatConfiguration"/> to communicate
+        /// with the LLM.
         return new ChatConfiguration(client, tokenCounter);
     }
 
     private static ChatConfiguration GetOpenAIChatConfiguration()
     {
+        /// Get an instance of Microsoft.Extensions.AI's <see cref="IChatClient"/> interface for the selected LLM
+        /// endpoint.
         IChatClient client =
             new OpenAIClient(EnvironmentVariables.OpenAIAPIKey)
                 .AsChatClient(modelId: EnvironmentVariables.OpenAIModel);
 
-        IEvaluationTokenCounter tokenCounter =
+        IEvaluationTokenCounter? tokenCounter = null;
+        if (EnvironmentVariables.OpenAIModelInputTokenLimit.HasValue)
+        {
+            /// Note that while <see cref="TiktokenTokenizer"/> supports a wide variety of models, it may not (yet)
+            /// support the model you have selected. In this case, you can either turn off the token counting for all
+            /// evaluations performed in the included examples (by unsetting the above environment variable that
+            /// specifies the token limit), or you can implement <see cref="IEvaluationTokenCounter"/> for the selected
+            /// model (by wrapping any other tokenizer API that that supports the selected model) and pass this
+            /// <see cref="IEvaluationTokenCounter"/> down to the <see cref="ChatConfiguration"/> created below.
+            tokenCounter =
             TiktokenTokenizer.CreateForModel(EnvironmentVariables.OpenAIModel)
-                .ToTokenCounter(EnvironmentVariables.OpenAIModelInputTokenLimit);
+                    .ToTokenCounter(EnvironmentVariables.OpenAIModelInputTokenLimit.Value);
+        }
 
+        /// Create an instance of Microsoft.Extensions.AI.Evaluation's <see cref="ChatConfiguration"/>. All the
+        /// evaluations performed in the included examples will use this <see cref="ChatConfiguration"/> to communicate
+        /// with the LLM.
         return new ChatConfiguration(client, tokenCounter);
     }
 }
